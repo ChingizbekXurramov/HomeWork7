76d366feddc41fe9e1797f5a5efd21967e7f77d7
Chingizbek <128830130+ChingizbekXurramov@users.noreply.github.com>
2023-04-04 21:02:26 +0300

    ARRY k-1=0 and k+1=0

---
+++ ARRAY k-1=0 --k+1=0/ARRAY k-1=0 --k+1=0/Program.cs
@@ -0,0 +1,57 @@
+﻿namespace ARRAY_k_1_0___k_1_0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            Console.WriteLine($"Enter ROW number=> ");
+            int row = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Enter COLUMN number=> ");
+            int column = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Enter K number=> ");
+            int k = int.Parse(Console.ReadLine());
+            int[][] arr = new int[row][];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = new int[column];
+                for (int j = 0; j < arr[i].Length; j++)
+                {
+                    arr[i][j] = random.Next(1, 5);
+                    Console.Write($" {arr[i][j]}");
+                }
+                Console.WriteLine();
+            }
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = 0; j < arr[i].Length; j++)
+                {
+                    if (k == arr[i][j])
+                    {
+                        if (k == arr[0][0])
+                        {
+                            continue;
+                        }
+                        if (k == arr[i][0])
+                        {
+                            arr[i - 1][arr[i].Length - 1] = 0;
+                        }
+                        else
+                        {
+                            arr[i][j - 1] = 0;
+                        }
+                    }
+                }
+            }
+            Console.WriteLine("After=> ");
+            for (int i = 0; i < row; i++)
+            {
+                for (int j = 0; j < column; j++)
+                {
+                    Console.Write($" {arr[i][j]}");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
